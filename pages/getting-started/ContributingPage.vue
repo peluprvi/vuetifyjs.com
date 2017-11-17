0d815ee09575568043fc4388c3142810863bc789
Ilian Iliev <iliyan.iliev@ymail.com>
2017-11-17 11:59:44 +0100

    done /getting-started/contributing

---
+++ pages/getting-started/ContributingPage.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+
+  page(
+    id="contributing"
+  )
+    page-introduction(
+      title="GettingStarted.Contributing.header"
+      desc="GettingStarted.Contributing.headerText"
+    )
+
+    section#reporting-issues
+      section-heading(value="GettingStarted.Contributing.issuesHeader")
+      div.mb-3
+        ul.browser-list
+          li(v-html="$t('GettingStarted.Contributing.issue1')")
+          li(v-html="$t('GettingStarted.Contributing.issue2')")
+          li(v-html="$t('GettingStarted.Contributing.issue3')")
+          li(v-html="$t('GettingStarted.Contributing.issue4')")
+          li(v-html="$t('GettingStarted.Contributing.issue5')")
+
+    section#pull-requests
+      section-heading(value="GettingStarted.Contributing.pullRequestsHeader")
+      div.mb-3
+        ul.browser-list
+          li(
+            v-for="n in 3"
+            :key="n"
+            v-html="$t(`GettingStarted.Contributing.pullRequest${n}`)"
+          )
+
+    section#local-dev
+      section-heading(value="GettingStarted.Contributing.localDevHeader")
+      section-text(value="GettingStarted.Contributing.localDevText1").mb-5
+      markup(lang="cli").mb-5
+        |git clone git@github.com:vuetifyjs/vuetify
+      section-text(value="GettingStarted.Contributing.localDevText2").mb-5
+      markup(lang="cli").mb-5
+        |git checkout dev
+        |npm install // or yarn
+        |npm run dev
+      section-text(value="GettingStarted.Contributing.localDevText3")
+
+</template>
