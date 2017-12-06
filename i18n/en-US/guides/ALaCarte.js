2d30480e67c3275602e5b12b204425b4028e81a3
adriaroms <adriaroms@gmail.com>
2017-12-06 21:03:40 +0100

    added a-la-carte simple guide with list of component and automatic generator ready to easily copy the selected elements

---
+++ i18n/en-US/guides/ALaCarte.js
@@ -0,0 +1,23 @@
+export default {
+  header: 'Overview',
+  headerText: 'This overview is designed to help you become fluent in **Vuetify\'s** A La Carte Importing. If you have not already installed the <code>a-la-carte</code> template, please do so <router-link to="/vuetify/quick-start">in the Quick start</router-link>.',
+  applicationHeader: 'Application',
+  applicationText1: 'Navigate to the **src** folder and open up <code>main.js</code>. We see that in the template come some components already configured to load.',
+  applicationText2: 'For example the code below tells the application that you use a <code>&lt;v-app&gt;, &lt;v-avigation-drawer&gt;, &lt;v-footer&gt; </code> and <code>&lt;v-toolbar&gt;</code>. Once defined, the markup can be used in any .vue file.',
+  componentNameListHeader: 'UI Component Name List',
+  componentNameListText1: 'We must be aware that components such as <code>VLayout</code> or <code>VFlex</code> are not defined as such, for organizational reasons are included within <code>VGrid</code>, to know what is the name of a component to import can review the following section',
+  toc: [
+    {
+      text: 'Overview',
+      href: 'introduction'
+    },
+    {
+      text: 'Application',
+      href: 'application'
+    },
+    {
+      text: 'UI Component Name List',
+      href: 'component-name-list'
+    },
+  ]
+}
