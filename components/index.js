190ebc048d1f20abe1839bb2f4858107e3cf92e4
nekosaur <albert@kaaman.nu>
2017-12-11 22:44:14 +0100

    fix: forgot to remove import

--- components/index.js
+++ components/index.js
@@ -2,7 +2,6 @@ import Core from '@/components/core'
 import Helpers from '@/components/helpers'
 import Misc from '@/components/misc'
 import Views from '@/components/views'
-import Generator from '@/components/generator'
 
 export default Object.assign({},
   Core,
