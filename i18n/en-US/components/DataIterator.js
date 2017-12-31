1cdf0f79226567d7da3a76cde9a108754ddae2f9
nekosaur <albert@kaaman.nu>
2017-12-31 10:21:32 +0100

    fix: switched to markdown

--- i18n/en-US/components/DataIterator.js
+++ i18n/en-US/components/DataIterator.js
@@ -1,11 +1,11 @@
 export default {
   header: 'Data iterator',
-  headerText: 'The <code>v-data-iterator</code> component is used for displaying data, and shares a majority of its functionality with the <code>v-data-table</code> component. Features include sorting, searching, pagination, inline-editing, header tooltips, and selection.',
+  headerText: 'The `v-data-iterator` component is used for displaying data, and shares a majority of its functionality with the `v-data-table` component. Features include sorting, searching, pagination, and selection.',
   components: ['v-data-iterator'],
   examples: [{
     simple: {
       header: 'Simple',
-      desc: 'The data-iterator allows you to customize exactly how to display your data. In this example we are using a grid list with cards. We can use the <code>content-tag</code> prop (along with <code>content-class</code> and <code>content-props</code> to specify what the wrapper element around the items should look like.'
+      desc: 'The `v-data-iterator` allows you to customize exactly how to display your data. In this example we are using a grid list with cards. We can use the `content-tag` prop (along with `content-class` and `content-props`) to specify what the wrapper element around the items should look like.'
     },
   }],
   props: {
