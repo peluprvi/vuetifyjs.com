a38754700351dc19b87bdef25222836e33692f04
ElderJames <shunjiey@hotmail.com>
2018-01-05 18:45:06 +0800

    update geting started

--- lang/zh_Hans/index.js
+++ lang/zh_Hans/index.js
@@ -1,6 +1,6 @@
 // Pages
 import Vuetify from './vuetify';
-// import GettingStarted from './getting-started';
+import GettingStarted from './getting-started';
 // import Guides from './guides';
 // import Layout from './layout';
 // import Style from './style';
@@ -15,15 +15,19 @@ import Vuetify from './vuetify';
 // // Directives
 // import Directives from './directives';
 
+// Mixins
+// import Mixins from './mixins'
+
 export default {
   // Components,
   // Directives,
   // Generic,
   // Layout,
+  // Mixins,
   // Style,
   // Motion,
   Vuetify,
-  // GettingStarted,
+  GettingStarted,
   // Guides
 };
 
