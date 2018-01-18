4f01b3b8e2abce5f2ae9fa503602ee55ba808315
Kael <kaelwd@gmail.com>
2018-01-18 21:01:28 +1100

    load language file based on url

--- i18n/index.js
+++ i18n/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from '@/lang/en'
+import * as langs from '@/lang'
 
 Vue.use(VueI18n)
 
-export function createI18n () {
+export function createI18n (locale) {
   return new VueI18n({
     locale: 'en',
     fallbackLocale: 'en',
     messages: {
-      en
+      en,
+      // TODO: lazy loading
+      [locale]: langs[locale]
     }
   })
 }
