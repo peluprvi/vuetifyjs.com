7f5be9fec05fd68e5220d171b41e2b67be6b8eff
John Leider <john.j.leider@gmail.com>
2018-01-21 20:19:35 -0500

    fix(initial language load in SSR): added conditionals in the i18n language loading for SSR

--- i18n/index.js
+++ i18n/index.js
@@ -5,14 +5,24 @@ import * as langs from '@/lang'
 
 Vue.use(VueI18n)
 
-export function createI18n (locale) {
+export function createI18n (ssrContext) {
+  let locale = 'en'
+
+  if (ssrContext && ssrContext.lang) {
+    locale = ssrContext.lang
+  } else if (typeof document !== 'undefined') {
+    locale = document.documentElement.lang
+  }
+
+  const messages = { en }
+
+  if (locale !== 'en' && langs[locale]) {
+    messages[locale] = langs[locale]
+  }
+
   return new VueI18n({
     locale: 'en',
     fallbackLocale: 'en',
-    messages: {
-      en,
-      // TODO: lazy loading
-      [locale]: langs[locale]
-    }
+    messages
   })
 }
