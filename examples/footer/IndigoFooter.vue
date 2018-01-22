4150e1778bdbaa36f6b7552feef1b4984f94460e
lee <lh.man@aol.com>
2018-01-22 09:56:14 -0500

    4 new footer examples

---
+++ examples/footer/IndigoFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer height="auto" class="d-block" >
+    <v-flex xs12 text-xs-center>
+      <v-card flat tile  class="indigo lighten-1 white--text">
+        <v-card-text  >
+          <v-spacer></v-spacer>
+            <v-btn
+             v-for="icon in icons"
+             :key="icon"
+             icon
+             class="mx-3 white--text"
+            >
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+        </v-card-text>
+      </v-card>
+      <v-card flat tile class="indigo lighten-1">
+       <v-card-text class="white--text">
+         Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+       </v-card-text>
+      </v-card>
+      <v-card flat tile class="indigo lighten-1">
+         <v-card-text class="white--text">
+            &copy; 2018 Copyright:
+            <strong>Vuetifyjs.com</strong>
+            <v-spacer></v-spacer>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: ['fa-facebook', 'fa-twitter', 'fa-google-plus', 'fa-linkedin', 'fa-instagram']
+    })
+  }
+</script>
---
+++ examples/footer/advancedfooter.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-footer height="auto" class="d-block">
+    <v-card flat tile>
+      <v-card-text class="white--text grey darken-2 ">
+        <v-layout>
+          <v-flex
+            v-for="(col, i) in rows"
+            :key="i"
+            xs3
+          >
+            <span class="body-2" v-text="col.title.toUpperCase()"></span>
+            <div
+              v-for="(child, i) in col.children"
+              :key="i"
+              v-text="child"
+            ></div>
+          </v-flex>
+        </v-layout>
+        <div class="white--text text-xs-center">
+          Register for free
+          <v-btn color="red accent-3" dark round large>
+            <v-icon dark></v-icon>SIGN UP!
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn class="white--text mx-3" fab color="blue">
+            <v-icon size="18px">fa-facebook</v-icon>
+          </v-btn>
+          <v-btn class="white--text mx-3" fab color="teal">
+            <v-icon size="18px">fa-twitter</v-icon>
+          </v-btn>
+          <v-btn class="white--text mx-3" fab color="green">
+            <v-icon size="18px">fa-google-plus</v-icon>
+          </v-btn>
+          <v-btn class="white--text mx-3" fab color="red">
+            <v-icon size="18px">fa-linkedin</v-icon>
+          </v-btn>
+          <v-btn class="white--text mx-3" fab color="pink">
+            <v-icon size="18px">fa-instagram</v-icon>
+          </v-btn>
+        </div>
+      </v-card-text>
+      <v-card-actions class="white--text grey darken-3">
+        <v-spacer></v-spacer>
+        <span>&copy; 2018 Copyright:</span>
+          <strong class="white--text">Vuetifyjs.com</strong>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      rows: [
+        {
+          title: 'FOOTER CONTENT',
+          children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
+        },
+        {
+          title: 'LINKS',
+          children: ['Link 1', 'Link 2', 'Link 3', 'Link 4']
+        },
+        {
+          title: 'LINKS',
+          children: ['Link 1', 'Link 2', 'Link 3', 'Link 4']
+        },
+        {
+          title: 'LINKS',
+          children: ['Link 1', 'Link 2', 'Link 3', 'Link 4']
+        }
+      ]
+    })
+  }
+</script>
