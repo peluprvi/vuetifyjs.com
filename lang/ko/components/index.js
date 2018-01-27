ac535cda859ecc605e3640a233d7683d59fb193c
qgp9 <pauljang9@gmail.com>
2018-01-27 17:35:13 +0200

    init translation for Korean

---
+++ lang/ko/components/index.js
@@ -0,0 +1,83 @@
+// TODO: import Alerts from './Alerts'
+// TODO: import Avatars from './Avatars'
+// TODO: import Badges from './Badges'
+// TODO: import BottomNavigation from './BottomNavigation'
+// TODO: import BottomSheets from './BottomSheets'
+// TODO: import Breadcrumbs from './Breadcrumbs'
+// TODO: import Buttons from './Buttons'
+// TODO: import Cards from './Cards'
+// TODO: import Carousels from './Carousels'
+// TODO: import Chips from './Chips'
+// TODO: import DataIterator from './DataIterator'
+// TODO: import DataTables from './DataTables'
+// TODO: import DatePickers from './DatePickers'
+// TODO: import Dialogs from './Dialogs'
+// TODO: import Dividers from './Dividers'
+// TODO: import ExpansionPanels from './ExpansionPanels'
+// TODO: import FloatingActionButtons from './FloatingActionButtons'
+// TODO: import Footer from './Footer'
+// TODO: import Forms from './Forms'
+// TODO: import Grid from './Grid'
+// TODO: import GridLists from './GridLists'
+// TODO: import Icons from './Icons'
+// TODO: import Lists from './Lists'
+// TODO: import Jumbotrons from './Jumbotrons'
+// TODO: import Menus from './Menus'
+// TODO: import NavigationDrawers from './NavigationDrawers'
+// TODO: import Paginations from './Paginations'
+// TODO: import Parallax from './Parallax'
+// TODO: import Progress from './Progress'
+// TODO: import SelectionControls from './SelectionControls'
+// TODO: import Selects from './Selects'
+// TODO: import Sliders from './Sliders'
+// TODO: import Snackbars from './Snackbars'
+// TODO: import Steppers from './Steppers'
+// TODO: import Subheaders from './Subheaders'
+// TODO: import Tabs from './Tabs'
+// TODO: import TextFields from './TextFields'
+// TODO: import TimePickers from './TimePickers'
+// TODO: import Toolbars from './Toolbars'
+// TODO: import Tooltips from './Tooltips'
+
+export default {
+// TODO:   Alerts,
+// TODO:   Avatars,
+// TODO:   Badges,
+// TODO:   BottomNavigation,
+// TODO:   BottomSheets,
+// TODO:   Breadcrumbs,
+// TODO:   Buttons,
+// TODO:   Cards,
+// TODO:   Carousels,
+// TODO:   Chips,
+// TODO:   DataIterator,
+// TODO:   DataTables,
+// TODO:   DatePickers,
+// TODO:   Dialogs,
+// TODO:   Dividers,
+// TODO:   ExpansionPanels,
+// TODO:   FloatingActionButtons,
+// TODO:   Footer,
+// TODO:   Forms,
+// TODO:   Grid,
+// TODO:   GridLists,
+// TODO:   Icons,
+// TODO:   Lists,
+// TODO:   Jumbotrons,
+// TODO:   Menus,
+// TODO:   NavigationDrawers,
+// TODO:   Paginations,
+// TODO:   Parallax,
+// TODO:   Progress,
+// TODO:   SelectionControls,
+// TODO:   Selects,
+// TODO:   Sliders,
+// TODO:   Snackbars,
+// TODO:   Steppers,
+// TODO:   Subheaders,
+// TODO:   Tabs,
+// TODO:   TextFields,
+// TODO:   TimePickers,
+// TODO:   Toolbars,
+// TODO:   Tooltips
+}
---
+++ lang/ko/index.js
@@ -0,0 +1,35 @@
+// Pages
+// TODO: import Vuetify from './vuetify'
+// TODO: import GettingStarted from './getting-started'
+// TODO: import Guides from './guides'
+// TODO: import Layout from './layout'
+// TODO: import Style from './style'
+// TODO: import Motion from './motion'
+
+// Generic
+// TODO: import Generic from './generic'
+
+// Components
+// TODO: import Components from './components'
+
+// Directives
+// TODO: import Directives from './directives'
+
+// Mixins
+// TODO: import Mixins from './mixins'
+
+// TODO: import meta from './meta'
+
+export default {
+//   Components,
+// TODO:   Directives,
+// TODO:   Generic,
+// TODO:   GettingStarted,
+// TODO:   Guides,
+// TODO:   Layout,
+// TODO:   Mixins,
+// TODO:   Motion,
+// TODO:   Style,
+// TODO:   Vuetify,
+// TODO:   meta
+}
