e23717a240b96257e9584d8a30ca5c23b5ea8ae0
lee <lh.man@aol.com>
2018-01-31 15:20:36 -0500

    fixes pr CI dividers

--- examples/dividers/DividerList.vue
+++ examples/dividers/DividerList.vue
@@ -1,66 +1,122 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs4 offset-md4 offset-sm1>
-      <v-card color="grey darken-1">
-        <v-card-title>
-          <span class="headline white--text pl-3">Portrait View</span>
+  <v-layout row>
+    <v-flex xs4 offset-md4 offset-sm3>
+      <v-card>
+        <v-toolbar class="orange lighten-1" dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>My List</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn dark icon>
-            <v-icon>chevron_left</v-icon>
+          <v-btn icon>
+            <v-icon>search</v-icon>
           </v-btn>
-          <v-btn dark icon>
-            <v-icon>edit</v-icon>
-          </v-btn>
-          <v-btn dark icon>
-            <v-icon>more_vert</v-icon>
-          </v-btn>
-        </v-card-title>
-        <v-list two-line class="grey darken-1">
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon color="white">phone</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="white--text">(650) 555-1234</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-icon class="white--text">chat</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-          <v-divider inset></v-divider>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon color="white">phone</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="white--text">(323) 555-6789</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-icon class="white--text">chat</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-          <v-divider inset></v-divider>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon color="white">mail</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="white--text">mcbeal@example.com</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <v-divider inset></v-divider>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-icon color="white">location_on</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title class="white--text">Orlando, FL 79938</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
+        </v-toolbar>
+        <v-list two-line class="orange lighten-2">
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              class="white--text">{{ item.header }}</v-subheader>
+            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+            <v-list-tile avatar v-else :key="item.title" >
+              <v-list-tile-avatar >
+                <img :src="item.avatar" >
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title" class="white--text"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle" class="white--text"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
         </v-list>
-        <v-card-media src="https://picsum.photos/250/300?image=1027" height="200px">
-        </v-card-media>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          header: "Today"
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=1027",
+          title: "Meeting @ Noon",
+          subtitle:
+            "<span class='text--primary'>Spike Lee</span> &mdash; I'll be in your neighborhood"
+        },
+        {
+          divider: true,
+          inset: true
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=821",
+          title: 'Summer BBQ <span class="grey--text text--lighten-1"></span>',
+          subtitle:
+            "<span class='text--primary'>to Operations support</span> &mdash; Wish I could come."
+        },
+        {
+          divider: true,
+          inset: true
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=783",
+          title: "Yes yes",
+          subtitle:
+            "<span class='text--primary'>Bella</span> &mdash; Do you have Paris recommendations"
+        },
+        {
+          header: "Tomorrow"
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=1006",
+          title: "Dinner tonight?",
+          subtitle:
+            "<span class='text--primary'>LaToya</span> &mdash; Do you want to hang out?"
+        },
+        {
+          divider: true,
+          inset: true
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=146",
+          title: "So long",
+          subtitle:
+            "<span class='text--primary'>Nancy</span> &mdash; Do you see what time it is?"
+        },
+        {
+          header: "Last Week"
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=1008",
+          title: "Breakfast?",
+          subtitle:
+            "<span class='text--primary'>LaToya</span> &mdash; Do you want to hang out?"
+        },
+        {
+          divider: true,
+          inset: true
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=839",
+          title:
+            'Winter Porridge <span class="grey--text text--lighten-1"></span>',
+          subtitle:
+            "<span class='text--primary'>cc: Daniel</span> &mdash; Tell me more..."
+        },
+        {
+          divider: true,
+          inset: true
+        },
+        {
+          avatar: "https://picsum.photos/250/300?image=145",
+          title: "Oui oui",
+          subtitle:
+            "<span class='text--primary'>Nancy</span> &mdash; Do you see what time it is?"
+        }
+      ]
+    };
+  }
+};
+</script>
