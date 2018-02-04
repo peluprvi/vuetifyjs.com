fd7f5b68f84bce2f9c17fef7607544b5caee37b7
dojineko <dojineko.spiral@gmail.com>
2018-02-04 23:37:34 +0900

    ja translation for BottomNavigation

--- lang/ja/components/BottomNavigation.js
+++ lang/ja/components/BottomNavigation.js
@@ -1,23 +1,23 @@
 export default {
   header: 'Bottom nav',
-  headerText: 'The `v-bottom-nav` is an alternative to the sidebar. It is primarily used on mobile and comes in two variants, icons and text, and shift.',
+  headerText: '`v-bottom-nav` はサイドバーの代わりに使用することができます。主にモバイルで使用され、アイコンとテキスト、そしてシフトの2種類が用意されています。',
   components: ['v-bottom-nav'],
   examples: [{
     iconsAndText: {
-      header: 'Icons and text',
-      desc: 'While the bottom nav is meant to be used with the `vue-router`, you can also programmatically control the active state of the buttons by using the `active.sync` prop. You can change a button\'s value by using its `value` attribute.'
+      header: 'アイコンとテキスト',
+      desc: 'Bottom nav は `vue-router` と一緒に使われることを想定していますが、`active.sync` を使用することでボタンのアクティブ状態をプログラム的に制御することができます。ボタンの値を変更するには `value` 属性を使用します。'
     },
     colorAndShift: {
-      header: 'Color & shift',
-      desc: 'When using a colored background, it is recommended to use the `light` prop. The `shift` prop will hide the button text until active. Keep in mind, `v-btn` text is required to be wrapped in a `<span>` tag.'
+      header: 'カラーとシフト',
+      desc: '色の付いた背景の場合、 `light` を指定することが推奨されます。`shift` を指定するとボタンがアクティブの間テキストを非表示にします。`v-btn` のテキストは `<span>` タグでラップしておくことが推奨されることを覚えておいてください。'
     },
     toggle: {
-      header: 'Toggle',
-      desc: 'As with other Vuetify components, you can control the display state with `v-model`.'
+      header: '切り替え',
+      desc: '他の Vuetify のコンポーネントと同じように、`v-model` を使用して表示するか否かを制御することができます。'
     }
   }],
   props: {
-    active: 'Holds the value of the currently active button. If the button has no value supplied, its index will be used instead. This prop supports the **.sync** modifier.',
+    active: '現在アクティブなボタンの値を保持します。特に値が設定されない場合は、indexの値が代わりに使用されます。この prop は **.sync** 修飾子をサポートします。',
     absolute: 'Mixins.Positionable.props.absolute',
     fixed: 'Mixins.Positionable.props.fixed'
   }
