53f3cb908687405f018e042f89238605104b37c0
dojineko <dojineko.spiral@gmail.com>
2018-02-04 22:04:24 +0900

    ja translation for Alerts

--- lang/ja/components/Alerts.js
+++ lang/ja/components/Alerts.js
@@ -1,32 +1,32 @@
 export default {
   header: 'Alert',
-  headerText: 'The `v-alert` component is used to convey important information to the user. It comes in 4 variations, **success**, **info**, **warning** and **error**. These have default icons assigned which can be changed and represent different actions',
+  headerText: '`v-alert` コンポーネントは重要な情報をユーザーに伝える際に使用します。 **success**、**info**、**warning** そして **error** の4つのバリエーションを備えています。 これらにはそれぞれ異なるアクションを表すデフォルトのアイコンが設定されており、必要に応じて変更が可能です。',
   components: ['v-alert'],
   examples: [{
     contextual: {
       header: 'Contextual'
     },
     closable: {
-      header: 'Closable',
-      desc: 'Using `v-model` you can control the state of the Alert. If you don\'t want to assign a v-model and just display the alert, you can simply assign the prop `value`.'
+      header: '閉じるボタン付きアラート',
+      desc: '`v-model` を使うことで、アラートの状態をコントロールすることができます。 v-model を使用せず、アラートを表示したいだけであれば、シンプルに `value` を使用することができます。'
     },
     icon: {
-      header: 'Custom Icon / No Icon',
-      desc: 'You can easily assign a custom icon or remove it all together.'
+      header: 'カスタムアイコン / アイコンの削除',
+      desc: 'カスタムアイコンの設定や削除は簡単に行うことができます。'
     },
     transition: {
-      header: 'Display transition',
-      desc: 'You can apply a custom transition to the show/hide of the alert.'
+      header: 'トランジション設定',
+      desc: 'Alert の表示/非表示の際にカスタムトランジションを適用することができます。'
     },
     outline: {
-      header: 'Outline',
-      desc: 'Outline alerts inherit their borders from the current color applied.'
+      header: 'アウトライン',
+      desc: 'アウトラインアラートは、適用されている色から引き継いだ色の境界線を表示します。'
     }
   }],
   props: {
-    dismissible: 'Specifies that the Alert can be closed',
-    icon: 'Designates a specific icon',
-    outline: 'Alert will have an outline',
-    type: 'Specify a **success**, **info**, **warning** or **error** alert. Uses the contextual color and a pre-defined icon.'
+    dismissible: 'アラートを閉じることができるかどうかを設定します。',
+    icon: 'アイコンを指定します。',
+    outline: '境界線を持つアラートを表示します。',
+    type: '**success**、**info**、**warning** もしくは **error** のアラートであることを設定します。コンテキストカラーと予め設定されたアイコンが使用されます。'
   }
 }
