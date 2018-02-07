6425a2f45ee11b2218f395ced89c6b736c088c3c
dojineko <dojineko.spiral@gmail.com>
2018-02-07 23:26:44 +0900

    ja translation for Chips

--- lang/ja/components/Chips.js
+++ lang/ja/components/Chips.js
@@ -1,46 +1,46 @@
 export default {
   header: 'Chip',
-  headerText: 'The `v-chip` component is used to convey small pieces of information. Using the `close` property, the chip becomes interactive, allowing user interaction.',
+  headerText: '`v-chip` コンポーネントは小さな情報の断片を伝えるために使用されます。`close` プロパティを使用すると、チップはインタラクティブとなり、ユーザーの入力を受け付けられるようになります。',
   components: ['v-chip'],
   examples: [{
     default: {
-      header: 'Default',
-      desc: 'Chips come in 4 primary variations. Regular, with icon, with portrait and closeable.',
+      header: 'デフォルト',
+      desc: 'チップには主な、基本型、アイコン付き、ポートレート付き、閉じるボタン付きの4種類が用意されています。',
       uninverted: true
     },
     colored: {
-      header: 'Colored',
-      desc: 'Any color from the Material Design palette can be used to change a chips color.'
+      header: '色付き',
+      desc: 'マテリアルデザインパレットのあらゆる色を使用してチップに色を付けることができます。'
     },
     icon: {
-      header: 'Icon',
-      desc: 'Chips can use text or any icon available in the Material Icons font library.'
+      header: 'アイコン',
+      desc: 'チップにはテキストのほか、Material Icons フォントに含まれるアイコンを使用することができます。'
     },
     outline: {
-      header: 'Outline',
-      desc: 'Outlined chips inherit their border color from the current text color.'
+      header: 'アウトライン',
+      desc: 'アウトラインチップは指定されているテキストの色を元にボーダーカラーが適用されます。'
     },
     label: {
-      header: 'Label',
-      desc: 'Label chips use the `v-card` border-radius.'
+      header: 'ラベル',
+      desc: 'ラベルチップは `v-card` の border-radius を使用します。'
     },
     closable: {
-      header: 'Closable',
-      desc: 'Closable chips can be controlled with a v-model. Listen to the `input` event if you want to know when a chip has been closed.',
+      header: '閉じるボタン付きラベル',
+      desc: '閉じるボタン付きラベルは v-model によって制御可能です。チップが閉じられたタイミングを検知したい場合は input イベントを使用してください。',
       uninverted: true
     },
     inSelects: {
-      header: 'In selects',
-      desc: 'Selects can use chips to display the selected data.',
+      header: '選択ボックスとの組み合わせ',
+      desc: '選択ボックスでは、選択された項目を表示するためにチップを使用することができます。',
       uninverted: true
     }
   }],
   props: {
-    close: 'Adds remove button',
-    disabled: 'Disables the chip, making it un-selectable',
-    label: 'Removes circle edges',
-    outline: 'Removes background and applies border and text color',
-    selected: 'Applies a selection color to the chip. Primarily used for displaying highlight context in `v-select`',
-    small: 'Decreases the chip size'
+    close: '閉じるボタンを追加します。',
+    disabled: 'チップを無効化し、選択できないようにします。',
+    label: '丸みを削除します。',
+    outline: '背景を削除しテキストカラーとボーダーカラーを適用します。',
+    selected: 'チップが選択された状態の色を適用します。 主に `v-select` でハイライト表示するために使用します。',
+    small: 'チップのサイズを縮小します。'
   }
 }
