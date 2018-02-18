716fec0ec70d8e34b94bf1ef27af8b92867ac172
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-02-18 17:52:30 -0300

    Removed en components from pt-br's folder.

--- lang/pt-BR/mixins/Bootable.js
+++
@@ -1,5 +0,0 @@
-export default {
-  props: {
-    lazy: 'Condicionalmente renderiza o conteúdo quando montado. Renderizará o conteúdo apenas se ele estiver ativado'
-  }
-}
--- lang/pt-BR/style/Typography.js
+++
@@ -1,5 +0,0 @@
-export default {
-  header: 'Tipografia',
-  headerText: 'A tipografia de uma aplicação é tão importante quanto sua funcionalidade. Vuetify.js usa o Design Material com aspecto da fonte "Roboto". Cada tamanho de título também possui uma classe auxiliar correspondente para estilizar outros elementos da mesma forma.',
-  examplesHeader: 'Exemplos'
-}
