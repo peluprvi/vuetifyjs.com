043e34e6ff148b74ba37201d9e2b9e9002fb9ce5
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-02-18 18:12:10 -0300

    Removed import.

--- lang/index.js
+++ lang/index.js
@@ -2,12 +2,10 @@ import en from './en'
 import ru from './ru'
 import ko from './ko'
 import zhHans from './zhHans'
-import pt-BR from './pt-BR'
 
 export {
   en,
   ru,
   ko,
-  zhHans,
-  pt-BR
+  zhHans
 }
