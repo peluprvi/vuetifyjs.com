31f88e347abdaf9b2dd9a68c4d5bf3c9c0ee0ea7
bad4iz <info@bad4iz.ru>
2018-03-09 10:28:41 +0400

    Footer

--- lang/ru/components/Footer.js
+++ lang/ru/components/Footer.js
@@ -1,28 +1,34 @@
 export default {
   header: 'Footer',
-  headerText: 'The `v-footer` component is used for displaying general information that a user might want to access from any page within your site.',
+  headerText:
+    '`v-footer`компонент используется для отображения общей информации, которую пользователь может захотеть получить с любой страницы вашего сайта.',
   components: ['v-footer'],
-  examples: [{
-    default: {
-      header: 'Default',
-      desc: 'The footer component is just a basic container.'
-    },
-    companyFooter: {
-      header: 'Company Footer',
-      desc: 'The footer component as a basic company footer with links.'
-    },
-    indigoFooter: {
-      header: 'Indigo Footer',
-      desc: 'The footer component with Indigo background color and social media icons and button.'
-    },
-    tealFooter: {
-      header: 'Teal Footer',
-      desc: 'The footer component with a Teal color header and columns and rows of links.'
+  examples: [
+    {
+      default: {
+        header: 'По умолчанию',
+        desc: 'Компонент footer - это просто базовый контейнер.'
+      },
+      companyFooter: {
+        header: 'Footer компании',
+        desc: 'Компонент footer как базовый footer компании со ссылками.'
+      },
+      indigoFooter: {
+        header: 'Indigo Footer',
+        desc:
+          'Компонент footer с цветом фона индиго и значками социальных сетей и кнопкой.'
+      },
+      tealFooter: {
+        header: 'Teal Footer',
+        desc:
+          'Компонент footer с заголовком цвета Teal и столбцами и строками ссылок.'
+      }
     }
-  }],
+  ],
   props: {
     absolute: 'Mixins.Positionable.props.absolute',
     fixed: 'Mixins.Positionable.props.fixed',
-    inset: 'Positions the toolbar offset from an application `v-navigation-drawer`'
+    inset:
+      'Позиционирует смещение toolbar (панели инструментов) от приложения `v-navigation-drawer`'
   }
 }
