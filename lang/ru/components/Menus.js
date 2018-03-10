451ab9beaa6e74de18e3abedc030a2b09b52f95f
bad4iz <info@bad4iz.ru>
2018-03-11 00:47:01 +0400

    Menus

--- lang/ru/components/Menus.js
+++ lang/ru/components/Menus.js
@@ -1,52 +1,52 @@
 export default {
   header: 'Menu',
-  headerText: 'The `v-menu` component shows a menu at the position of the element used to activate it.',
+  headerText: 'Компонент `v-menu` показывает меню в позиции элемента, используемого для его активации.',
   components: ['v-menu'],
   examples: [{
     activator: {
       header: 'Activator',
-      desc: 'Remember to put the element that activates the menu in the `activator` slot.',
+      desc: 'Не забудьте поставить элемент, который активирует меню в `activator` slot.',
       uninverted: true
     },
     absolute: {
-      header: 'Absolute position',
-      desc: 'Menus can also be placed absolutely on top of the activator element using the `absolute` prop. Try clicking anywhere on the image.',
+      header: 'Абсолютное положение',
+      desc: 'Меню также могут быть помещены абсолютно поверх элемента активатора с помощью `absolute` prop. Попробуйте щелкнуть в любом месте изображения.',
       uninverted: true
     },
     absoluteWithoutActivator: {
-      header: 'Absolute position without activator',
-      desc: 'Menus can also be used without an activator by using `absolute` together with the props `position-x` and `position-y`. Try right-clicking anywhere on the image.',
+      header: 'Абсолютное положение без активатора',
+      desc: 'Меню также можно использовать без активатора, используя `absolute` вместе с реквизитами `position-x` и `position-y`. Попробуйте щелкнуть правой кнопкой мыши в любом месте изображения.',
       uninverted: true
     },
     hover: {
       header: 'Hover',
-      desc: 'Menus can be accessed using hover instead of clicking with the `open-on-hover` prop.',
+      desc: 'Доступ к меню возможен с помощью наведения, а не щелчка `open-on-hover` prop.',
       uninverted: true
     },
     menus: {
-      header: 'Menus',
-      desc: 'Menus can be placed within almost any component.',
+      header: 'Меню',
+      desc: 'Меню можно размещать практически в любом компоненте.',
       uninverted: true
     },
     customTransition: {
-      header: 'Custom transitions',
-      desc: 'Vuetify comes with 3 standard transitions, **scale**, **slide-x** and **slide-y**. You can also create your own and pass it as the transition argument. For an example of how the stock transitions are constructed, visit <a href="https://github.com/vuetifyjs/vuetify/blob/master/src/util/helpers.js#L13" target="_blank" rel="noopener">here</a>.',
+      header: 'Пользовательские переходы',
+      desc: 'Vuetify поставляется с 3 стандартными переходами, **scale**, **slide-x** и **slide-y**. Вы также можете создать свой собственный и передать его в качестве аргумента перехода. Для примера того, как построены стоковые переходы,<a href="https://github.com/vuetifyjs/vuetify/blob/master/src/util/helpers.js#L13" target="_blank" rel="noopener"> посетите это</a>.',
       uninverted: true
     },
     popover: {
       header: 'Popover menu',
-      desc: 'A menu can be configured to be static when opened, allowing it to function as a popover. This can be useful when there are multiple interactive items within the menu contents.',
+      desc: 'Меню можно настроить как статическое при открытии, что позволяет ему функционировать как popover. Это может быть полезно, когда в содержимом меню есть несколько интерактивных элементов.',
       uninverted: true
     }
   }],
   props: {
-    closeOnClick: 'Designates if menu should close on outside-activator click',
-    closeOnContentClick: 'Designates if menu should close when its content is clicked',
-    disabled: 'Disables the menu',
-    offsetX: 'Offset the menu on the x-axis. Works in conjunction with direction left/right',
-    offsetY: 'Offset the menu on the y-axis. Works in conjunction with direction top/bottom',
-    openOnClick: 'Designates whether menu should open on activator click',
-    openOnHover: 'Designates whether menu should open on activator hover',
+    closeOnClick: 'Указывает, должен ли меню закрываться на внешнем активаторе.',
+    closeOnContentClick: 'Указывает, должен ли меню закрываться при нажатии на его содержимое',
+    disabled: 'Отключает меню',
+    offsetX: 'Сдвиньте меню по оси x. Работает вместе с направлением влево / вправо',
+    offsetY: 'Смещение меню по оси y. Работает вместе с направлением сверху / снизу',
+    openOnClick: 'Указывает, следует ли открывать меню при нажатии активатора',
+    openOnHover: 'Указывает, следует ли открывать меню при наведении активатора',
     origin: 'Mixins.Transitionable.props.origin',
     transition: 'Mixins.Transitionable.props.transition'
   }
