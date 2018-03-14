ec6191c0faf8c5bd53fa5981b0d7007b9616eda5
bad4iz <info@bad4iz.ru>
2018-03-14 21:35:49 +0400

    TemplateList

--- lang/ru/components/TemplateList.js
+++ lang/ru/components/TemplateList.js
@@ -2,43 +2,43 @@ export default {
   close: 'Close',
   init: 'Init command copied',
   quickstart: {
-    title: 'One Minute Quickstart',
-    desc: 'Useful for quickly previewing the capabilities of the Vuetify framework. Because Vuetify can be bound at run-time. Even in environments with no build process, you can create beautiful model bound templates.'
+    title: 'Одноминутный быстрый старт',
+    desc: 'Полезно для быстрого просмотра возможностей инфраструктуры Vuetify. Поскольку Vuetify может быть привязан во время выполнения. Даже в средах без процесса сборки вы можете создавать красивые шаблоны, связанные с шаблонами.'
   },
   simple: {
-    title: 'Simple HTML',
-    desc: 'This template is intended for users who want to try out Vue.js and Vuetify.js in the most simplistic way. It contains a basic index.html with no additional functionality. This is useful for developers who want to easily preview the features of the framework.'
+    title: 'Простой HTML',
+    desc: 'Этот шаблон предназначен для пользователей, которые хотят попробовать Vue.js и Vuetify.js самым простым способом. Он содержит базовый index.html без дополнительной функциональности. Это полезно для разработчиков, которые хотят легко просмотреть функции фреймворка.'
   },
   webpack: {
     title: 'Webpack',
-    desc: 'This template is intended for users who are looking for out of the box linting and unit testing.'
+    desc: 'Этот шаблон предназначен для пользователей, которые ищут из коробки линтинг и модульное тестирование.'
   },
   webpackSimple: {
     title: 'Webpack Simple',
-    desc: 'This template is intended for users who are already familiar with Vue/Webpack. It contains a very simple webpack setup and is targetted at developers creating prototype or basic applications.'
+    desc: 'Этот шаблон предназначен для пользователей, уже знакомых с Vue / Webpack. Он содержит очень простую настройку webpack и предназначен для разработчиков, создающих прототипы или базовые приложения.'
   },
   webpackSSR: {
     title: 'Webpack SSR',
-    desc: 'This template is for advanced users looking to utilize the new Vue SSR (server-side rendering). Based off of the structure in the Vue.js 2 <a class="white--text" href="https://github.com/vuejs/vue-hackernews-2.0" target="_blank" rel="noopener">Hackernews</a> repository. The Vuetify.js SSR template provides next generation functionality for advanced Vue applications.'
+    desc: 'Этот шаблон предназначен для опытных пользователей, которые хотят использовать новый Vue SSR (рендеринг на стороне сервера). Исходя из структуры в Vue.js 2 <a class="white--text" href="https://github.com/vuejs/vue-hackernews-2.0" target="_blank" rel="noopener"></a> репозиторий. Шаблон SSR Vuetify.js обеспечивает функциональность следующего поколения для расширенных приложений Vue.'
   },
   nuxt: {
     title: 'NUXT',
-    desc: 'Utilizing the power of NUXT, supercharge your development experience with a bootstraped version ready to go with Vuetify out of the box.'
+    desc: 'Используя силу NUXT, переведите свой опыт разработки на новый уровень с помощью загрузочной версии, готовой к работе с Vuetify из коробки.'
   },
   pwa: {
     title: 'PWA',
-    desc: 'A pre-configured PWA (Progressive Web Application) template is at your disposal. Bootstraped with service workers, application manifest, and a 90+/100 Lighthouse score.'
+    desc: 'В вашем распоряжении предварительно настроенный шаблон PWA (прогрессивное веб-приложение). Загрузитесь с service workers, манифестами приложений и 90 + / 100 Lighthouse score .'
   },
   electron: {
     title: 'Electron',
-    desc: 'Vuetify\'s official Electron template for creating desktop applications.'
+    desc: 'Vuetify\'s официальный Electron шаблон для создания настольных приложений.'
   },
   aLaCarte: {
     title: 'A La Carte',
-    desc: 'In this template you can see an example of how to select only the components you want to use. This is useful for reducing package size with unused components.'
+    desc: 'В этом шаблоне вы можете увидеть пример выбора только тех компонентов, которые вы хотите использовать. Это полезно для уменьшения размера пакета с неиспользуемыми компонентами.'
   },
   cordova: {
     title: 'Cordova',
-    desc: 'This template uses Apache Cordova to help you easily convert your web app into a native Android or iOS app.'
+    desc: 'Этот шаблон использует Apache Cordova, чтобы помочь вам легко конвертировать ваше веб-приложение в родное приложение Android или iOS.*'
   }
 }
