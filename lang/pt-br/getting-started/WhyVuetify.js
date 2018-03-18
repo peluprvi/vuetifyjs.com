34725998be5fa67825f04662d62e6152473237ff
Pedro Vieira <peluprvi@gmail.com>
2018-03-18 18:05:15 -0300

    Review pt-BR i18n of WhyVuetify.js

--- lang/pt-br/getting-started/WhyVuetify.js
+++ lang/pt-br/getting-started/WhyVuetify.js
@@ -4,26 +4,26 @@ export default {
   toc: [
     {
       text: `Qual é a diferença?`,
-      href: 'porque-vuetify'
+      href: 'why-vuetify'
     },
     {
       text: 'Princípios de design',
-      href: 'principios-de-design'
+      href: 'design-principles'
     },
     {
       text: 'Comunidade vibrante',
-      href: 'comunitade-vibrante'
+      href: 'vibrant-community'
     },
     {
-      text: 'Comparação da framework',
-      href: 'comparacao-da-framework'
+      text: 'Comparação do framework',
+      href: 'framework-comparison'
     }
   ],
   philosophies: [
     {
       img: '/static/vuetify_semantic.svg',
       title: 'Kit de ferramentas semântico',
-      caption: `O Vuetify foi feito com o intuito de ser semântico. Fácil de aprender, fácil de lembrar &mdash; Isso significa que se você está desenvolvendo seu próprio aplicativo, ou iniciando um novo desenvolvedor, os componentes e propriedades _escreve-como-se-fala_ do Vuetify faz o desenvolvimento ficar supimpa.`
+      caption: `O Vuetify foi feito com o intuito de ser semântico. Fácil de aprender, fácil de lembrar &mdash; Isso significa que se você está desenvolvendo seu próprio aplicativo, ou recebendo um novo desenvolvedor, os componentes e propriedades _escreve-como-se-fala_ do Vuetify faz o desenvolvimento ser simples.`
     },
     {
       img: '/static/vuetify_sizes.svg',
@@ -37,29 +37,29 @@ export default {
     }
   ],
   whyText: [
-    'Escolher uma estrutura pode ser uma tarefa assustadora, e nem sequer falo sobre frameworks de componentes. Você chegou ao Vue e agora é hora de começar a desenvolver sua próxima ótima idéia. Existem muitas opções para escolher, e todos dizem que são excelentes. Então você tem que decidir se perguntando: "Quais são minhas métricas para o sucesso?"',
-    'Isso  foi comigo a quase um ano atrás. Havia muitas **Ferramentas para UI** para escolher, mas nenhuma delas tinha tudo o que eu precisava... E sim, você já passou por antes. E é por isso que comecei a trabalhar no Vuetify.',
-    'Mas agora você é daqueles que procura esse quadro perfeito, o Vuetify é outro em uma lista de muitos, competindo por sua atenção. Embora minha opinião seja subjetiva, vou tentar descrever recursos que eu acredito que serão valiosos para você e/ou sua organização.'
+    'Escolher um framework pode ser uma tarefa assustadora, e nem sequer falo sobre frameworks de componentes. Você chegou ao Vue e agora é hora de começar a desenvolver sua próxima ótima idéia. Existem muitas opções para escolher, e todos dizem que são excelentes. Então você tem que decidir se perguntando: "Quais são minhas métricas para o sucesso?"',
+    'Isso foi comigo a quase um ano atrás. Havia muitas **Ferramentas para UI** para escolher, mas nenhuma delas tinha tudo o que eu precisava... E sim, você já ouviu isso antes. Muitas vezes, porém, _esta_ é a razão por tentativas individuais de criar produtos que preenchem lacunas e é por isso que eu comecei a trabalhar no Vuetify.',
+    'Mas agora você é um daqueles que procura esse framework perfeito, e o Vuetify é outro em uma lista de muitos, competindo por sua atenção. Embora minha opinião seja subjetiva, vou tentar descrever recursos que eu acredito que serão valiosos para você e/ou sua organização.'
   ],
   designHeader: 'Princípios de design',
-  designText: 'O Vuetify é desenvolvido exatamente de acordo com as especificações de Material Design. Cada componente é trabalhado manualmente para oferecer as melhores ferramentas UI possíveis para o seu próximo ótimo aplicativo. O desenvolvimento não para nos componentes principais delineados nas especificações do Google. Através do apoio de membros da comunidade e patrocinadores, componentes adicionais serão projetados e disponibilizados para que todos possam desfrutar.',
+  designText: 'O Vuetify é desenvolvido exatamente de acordo com as especificações de Material Design. Cada componente é trabalhado manualmente para oferecer as melhores ferramentas UI possíveis para o seu ótimo próximo aplicativo. O desenvolvimento não para nos componentes principais delineados nas especificações do Google. Através do apoio de membros da comunidade e patrocinadores, componentes adicionais serão projetados e disponibilizados para que todos possam desfrutar.',
   communityHeader: 'Comunidade Vibrante',
-  communityText: `Desenvolver com Vuetify significa que você nunca estará sozinho. Obtenha ajuda imediatamente da equipe de desenvolvimento e outros membros da comunidade no servidor oficial do Discord. Tenha a oportunidade de falar com indivíduos talentosos que desejam que você tenha sucesso em seus esforços. Precisa de suporte premium? Torne-se um <a href="https://www.patreon.com/vuetify" target="_blank" rel="noopener"> patrocinador Patreon </a> e tenha acesso aos canais de suporte #patron ou #elite-patron.`,
+  communityText: `Desenvolver com Vuetify significa que você nunca estará sozinho. Obtenha ajuda imediatamente da equipe de desenvolvimento e outros membros da comunidade no servidor oficial do Discord. Tenha a oportunidade de falar com indivíduos talentosos que desejam que você tenha sucesso em seus esforços. Precisa de suporte premium? Torne-se um <a href="https://www.patreon.com/vuetify" target="_blank" rel="noopener">patrocinador no Patreon</a> e tenha acesso aos canais de suporte #patron ou #elite-patron.`,
   comparisonHeader: 'Comparação com outros frameworks',
-  comparisonText: 'O Vuetify não é a unica framework do material para Vue. Muitos outros desenvolvedores talentosos criaram algumas aplicações surpreendentes sob o pretexto das especificações de design do material do Google. Cada um oferece sua própria tomada única no desenvolvimento e processo, no entanto, aqui estão algumas coisas que separam o Vuetify:',
-  featuresHeader: 'O que se tem:',
+  comparisonText: 'O Vuetify não é o unico framework de material para Vue. Muitos outros desenvolvedores talentosos criaram algumas aplicações surpreendentes na aparência das especificações do Google Material Design. Cada um oferece sua própria visão no desenvolvimento e processo, no entanto, aqui estão algumas coisas que separam o Vuetify:',
+  featuresHeader: 'O que você ganha:',
   featuresList: [
     'Componentes Auxiliares Semânticos',
     'Suporte ao SSR e PWA',
     '8 modelos via Vue-CLI',
-    'Suporte via bate-papo',
+    'Suporte via chat',
     'Sistema de Transição de Componente Modular',
-    'Blazing Fast Framework',
-    'Componentes à la carte'
+    'Framework Super Rápido',
+    'Componentes à La Carte'
   ],
-  letterFromAuthor: 'I am extremely humbled that you have considered using Vuetify as your next UI Framework. A lot of care has went into forging the perfect setup for taking any idea from conception to production. I look forward to you joining the Vuetify community and am excited to see what incredible interfaces you create!',
-  authorOfVuetify: 'Author do Vuetify',
-  contactMe: 'Entre em contrato comigo',
+  letterFromAuthor: 'Eu estou extremamente honrado que você considerou usar o Vuetify como seu próximo Framework de UI. Muito cuidado é colocado para forjar a configuração perfeita para tirar qualquer ideia desde a concepção até a produção. Estou ansioso para se juntar à comunidade Vuetify e estou ansioso para ver quais interfaces incríveis você cria!',
+  authorOfVuetify: 'Autor do Vuetify',
+  contactMe: 'Contate-me',
   questionHeader: 'Ainda tem perguntas?',
   questionButton: 'Pergunte à comunidade'
 }
