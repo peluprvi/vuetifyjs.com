706fa54c438caeff886e4f573af30b4a39e2cc54
John Leider <john.j.leider@gmail.com>
2018-06-03 14:43:44 -0400

    refactor(v-badge): update examples

--- examples/badges/visibility.vue
+++ examples/badges/visibility.vue
@@ -1,15 +1,37 @@
 <template>
   <v-container fluid class="text-xs-center">
-    <v-layout flex wrap row justify-space-between>
+    <v-layout
+      justify-space-between
+      row
+      wrap
+    >
       <v-flex xs12>
-        <v-btn @click.native="show = !show">toggle</v-btn>
+        <v-btn
+          color="indigo"
+          class="white--text"
+          @click="show = !show"
+        >
+          Toggle
+        </v-btn>
       </v-flex>
+
       <v-flex xs12 class="mt-5">
-        <v-badge v-model="show" color="cyan" left>
+        <v-badge
+          v-model="show"
+          color="cyan"
+          left
+        >
           <span slot="badge">6</span>
-          <v-icon large color="grey lighten-1">shopping_cart</v-icon>
+          <v-icon
+            large
+            color="grey lighten-1"
+          >shopping_cart</v-icon>
         </v-badge>
-        <v-badge v-model="show" color="indigo">
+
+        <v-badge
+          v-model="show"
+          color="purple"
+        >
           <span slot="badge">6</span>
           <v-icon large color="grey">mail</v-icon>
         </v-badge>
@@ -22,13 +44,7 @@
   export default {
     data () {
       return {
-        show: true,
-        badgeData: { value: '!' }
-      }
-    },
-    computed: {
-      badge () {
-        return this.show ? this.badgeData : null
+        show: true
       }
     }
   }
