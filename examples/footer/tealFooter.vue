9257075dd92e26d478a02be30b3dbada978500ba
John Leider <john.j.leider@gmail.com>
2018-06-03 19:24:54 -0400

    refactor(v-footer): update examples

--- examples/footer/tealFooter.vue
+++ examples/footer/tealFooter.vue
@@ -1,55 +1,30 @@
 <template>
-  <v-footer height="auto">
-    <v-card flat tile class="flex">
-      <v-card-title class="teal white--text">
+  <v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
         <strong class="subheading">Get connected with us on social networks!</strong>
+
         <v-spacer></v-spacer>
+
         <v-btn
           v-for="icon in icons"
           :key="icon"
-          icon
-          dark
           class="mx-3"
+          dark
+          icon
         >
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
       </v-card-title>
-      <v-card-text class="grey lighten-3">
-        <v-layout>
-          <v-flex
-            v-for="(col, i) in rows"
-            :key="i"
-            xs3
-          >
-            <span class="body-2" v-text="col.title.toUpperCase()"></span>
-            <div
-              v-for="(child, i) in col.children"
-              :key="i"
-              v-text="child"
-            ></div>
-          </v-flex>
-          <v-flex xs3 layout column>
-            <span class="body-2">CONTACT</span>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-home</v-icon>
-              New York, NY 10012, US
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-envelope</v-icon>
-              info@example.com
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-phone</v-icon>
-              + 01 234 567 88
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-print</v-icon>
-              + 01 234 567 89
-            </div>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-      <v-card-actions class="grey lighten-2 justify-center">
+
+      <v-card-actions class="grey darken-3 justify-center">
         &copy;2018 — <strong>Vuetify</strong>
       </v-card-actions>
     </v-card>
@@ -59,20 +34,12 @@
 <script>
   export default {
     data: () => ({
-      icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
-      rows: [
-        {
-          title: 'Company Name',
-          children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
-        },
-        {
-          title: 'Products',
-          children: ['MDBootstrap', 'MDWordPress', 'BrandFlow', 'Bootstrap Angular']
-        },
-        {
-          title: 'Useful Links',
-          children: ['Your account', 'Become an Affiliate', 'Shipping Rates', 'Helper']
-        }
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
       ]
     })
   }
