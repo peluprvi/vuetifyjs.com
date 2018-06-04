a303eab4fb03af5610fa6598b9448db11d910a7c
John Leider <john.j.leider@gmail.com>
2018-06-03 22:51:00 -0400

    refactor(v-toolbar): update examples

--- examples/toolbars/flexibleAndCard.vue
+++ examples/toolbars/flexibleAndCard.vue
@@ -1,25 +1,37 @@
 <template>
   <v-card flat>
-    <v-toolbar dark color="primary" flat extended>
+    <v-toolbar
+      color="primary"
+      dark
+      extended
+      flat
+    >
       <v-toolbar-side-icon></v-toolbar-side-icon>
     </v-toolbar>
+
     <v-layout row pb-2>
       <v-flex xs8 offset-xs2>
         <v-card class="card--flex-toolbar">
           <v-toolbar card prominent>
             <v-toolbar-title class="body-2 grey--text">Title</v-toolbar-title>
+
             <v-spacer></v-spacer>
+
             <v-btn icon>
               <v-icon>search</v-icon>
             </v-btn>
+
             <v-btn icon>
               <v-icon>apps</v-icon>
             </v-btn>
+
             <v-btn icon>
               <v-icon>more_vert</v-icon>
             </v-btn>
           </v-toolbar>
+
           <v-divider></v-divider>
+
           <v-card-text style="height: 200px;"></v-card-text>
         </v-card>
       </v-flex>
