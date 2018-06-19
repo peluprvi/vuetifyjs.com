a1ba8d26616f57f3cc2e2b8a145c12fc75dfc45e
John Leider <john@vuetifyjs.com>
2018-06-19 10:07:18 -0500

    feat(i18n): update docs (#499)
    * feat(i18n): update docs
    
    connected to #4319
    
    * fix: updated language and example

--- src/pages/framework/InternationalizationPage.vue
+++ src/pages/framework/InternationalizationPage.vue
@@ -54,6 +54,47 @@
         |   &lt;/div&gt;
         | &lt;/template&gt;
 
+    section#vue-i18n
+      section-head(value="Framework.Internationalization.vueI18nHeader")
+      section-text(value="Framework.Internationalization.vueI18nText1")
+      section-text(value="Framework.Internationalization.vueI18nText2")
+
+      markup(lang="js")
+        | import VueI18n from 'vue-i18n'
+
+        | Vue.use(VueI18n)
+
+        | const messages = {
+        |   en: {
+        |     $vuetify: {
+        |       dataIterator: {
+        |         rowsPerPageText: 'Items per page:',
+        |         pageText: '{0}-{1} of {2}'
+        |       }
+        |     },
+        |   },
+        |   sv: {
+        |     $vuetify: {
+        |       dataIterator: {
+        |         rowsPerPageText: 'Element per sida:',
+        |         pageText: '{0}-{1} av {2}'
+        |       }
+        |     },
+        |   }
+        | }
+
+        | // Create VueI18n instance with options
+        | const i18n = new VueI18n({
+        |   locale: 'sv', // set locale
+        |   messages, // set locale messages
+        | })
+
+        | Vue.use(Vuetify, {
+        |   lang: {
+        |     t: (key, ...params) => i18n.t(key, params)
+        |   }
+        | })
+
 </template>
 
 <script>
