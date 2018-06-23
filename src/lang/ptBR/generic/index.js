21b5ea6b46b5a4f7c2f7d1407b2761d2bdea28af
Pedro Luiz Prado Vieira <peluprvi@gmail.com>
2018-06-23 12:35:13 -0300

    feat(i18n): add props and slots pt-BR translations

--- src/lang/ptBR/generic/index.js
+++ src/lang/ptBR/generic/index.js
@@ -2,10 +2,14 @@ import Common from './Common'
 import Events from './Events'
 import Pages from './Pages'
 import Params from './Params'
+import Props from './Props'
+import Slots from './Slots'
 
 export default {
   Common,
   Events,
   Pages,
-  Params
+  Params,
+  Props,
+  Slots
 }
