3a7e7e9735dfef4b46ca584f3f06cb7634a4a24a
Sean Kimball <smkimbal@gmail.com>
2018-07-31 07:15:12 -0700

    Docs/date picker multiple (#560)
    * Add component examples for multiple prop
    
    * Update jsons for new prop and add lang
    
    * fix(v-date-picker): add newIn

---
+++ src/examples/date-pickers/dateMultiple.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-date-picker
+      v-model="dates"
+      multiple
+    ></v-date-picker>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dates: ['2018-09-15', '2018-09-20']
+    })
+  }
+</script>
